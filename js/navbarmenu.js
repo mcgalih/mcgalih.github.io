0a8ecd9868c24dd582a4e6527495c9bdb0027194
mcgalih <mcgalihramdhan@gmail.com>
2023-06-06 19:23:30 +0700

    function for responsive navbar

--- js/navbarmenu.js
+++ js/navbarmenu.js
@@ -1,8 +1,26 @@
-function toggle() {
-    var x = document.getElementById("navbar_menus");
+function toggle_hamb() {
+    let x = document.getElementById("navbar_menus");
     if (x.style.display === "none") {
-    x.style.display = "flex";
+        x.style.display = "flex";
     } else {
-    x.style.display = "none";
+        x.style.display = "none";
     }
 }
+
+function toggle_attr() {
+    let x = document.getElementById("navbar_menus");
+    if (screen.matches) {
+        if (x.style.display !== "flex") {
+            x.style.display = "flex";
+        }
+    } else {
+        if (x.style.display === "none") {
+            x.style.display = "flex";
+        } else {
+            x.style.display = "none";
+        }
+    }
+}
+
+var screen = window.matchMedia("(min-width: 1024px)")
+screen.addListener(toggle_attr)
